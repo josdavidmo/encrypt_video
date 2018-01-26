873780990544380bace114969d8d16479c972c9a
José David Moreno Posada <josemoreno@ticsocial.com.co>
2018-01-26 15:37:33 -0500

    add histogram real time

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from classes import Lorenz
 from classes import Protocol
+from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 import time
@@ -13,14 +14,24 @@ sender = Protocol(master)
 receiver = Protocol(slave)
 
 key = sender.get_sequence(25)
-print receiver.synchronize(key)
+receiver.synchronize(key)
+
+plt.ion()
 
 while(True):
+    plt.clf()
+
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     start_time = time.time()
     frame = sender.encrypt(frame)
+    color = ('b', 'g', 'r')
+    for i, col in enumerate(color):
+        histr = cv2.calcHist([frame], [i], None, [256], [0, 256])
+        plt.plot(histr, color=col)
+        plt.xlim([0, 256])
+    plt.show()
     print("--- Encrypt %s seconds ---" % (time.time() - start_time))
     start_time = time.time()
     frame = receiver.decrypt(frame)
@@ -29,7 +40,9 @@ while(True):
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    plt.pause(0.1)
 
 # When everything done, release the capture
+plt.ioff()
 cap.release()
 cv2.destroyAllWindows()
